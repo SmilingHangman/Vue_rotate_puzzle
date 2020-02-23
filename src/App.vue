579eb9ddf71ceb7434a91ff212a9f5a88dcccad2
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-02-24 00:07:23 +0200

    puzzlebox component added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PuzzleBox></PuzzleBox>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PuzzleBox from '@/components/PuzzleBox'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PuzzleBox
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
 </style>
---
+++ src/components/PuzzleBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="puzzle-box"></div>
+</template>
+
+<script>
+export default {
+  name: 'PuzzleBox'
+}
+</script>
+
+<style>
+  .puzzle-box {
+    margin: 20px auto 0 auto;
+    min-width: 335px;
+    min-height: 335px;
+    border: 5px ridge rgb(99, 41, 85);
+    background-color: rgba(161, 133, 155, 0.63);
+  }
+</style>
