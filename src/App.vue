eca3951623cf6922f0c4b64735b0b000016f22aa
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-02-24 16:34:09 +0200

    button added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <PuzzleBox></PuzzleBox>
+    <BaseButton>Get a Pic</BaseButton>
   </div>
 </template>
 
 <script>
 import PuzzleBox from '@/components/PuzzleBox'
+import BaseButton from '@/components/BaseButton'
 export default {
   name: 'App',
   components: {
-    PuzzleBox
+    PuzzleBox,
+    BaseButton
   }
 }
 </script>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    @click="onClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  methods: {
+    onClick (event) {
+      if (!this.disabled) {
+        this.$emit('click', event)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  button {
+    font-family: 'Mansalva', cursive;
+    font-size: 12px;
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    text-align: center;
+    color: pink;
+    background-color: rgb(99, 41, 85);
+    border-top-color:  rgb(209, 147, 195);
+    border-left-color: rgb(209, 147, 195);
+    border-right-color: rgb(207, 163, 197);
+    border-bottom-color: rgb(207, 163, 197);
+  }
+</style>
